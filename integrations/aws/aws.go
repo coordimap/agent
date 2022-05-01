99cbb273be21f27ae133d17d57abd04501a7ea55
Ermal Guni <ermalguni@gmail.com>
2022-05-01 14:23:41 +0200

    Crawl all eks clusters

--- integrations/aws/aws.go
+++ integrations/aws/aws.go
@@ -157,6 +157,9 @@ func (awsCrawl *AwsCrawl) crawl() (*bloopi_agent.CloudCrawlData, error) {
 
 		wg.Add(1)
 		go worker("rds", owner, regionSession, results, &wg)
+
+		wg.Add(1)
+		go worker("eks", owner, regionSession, results, &wg)
 	}
 
 	go func() {
