34e7f928fc83d347677addb12499e3d30fade425
Ermal Guni <ermalguni@gmail.com>
2024-12-21 09:37:56 +0100

    Add status for the sql instance

--- integrations/gcp/status.go
+++ integrations/gcp/status.go
@@ -4,13 +4,13 @@ import "dev.azure.com/bloopi/bloopi/_git/shared_models.git/bloopi_agent"
 
 func getComputeStatus(status string) string {
 	switch status {
-	case "RUNNING", "READY":
+	case "RUNNING", "READY", "RUNNABLE":
 		return bloopi_agent.StatusGreen
 
-	case "DEPROVISIONING", "STOPPED", "STOPPING", "SUSPENDED", "SUSPENDING", "TERMINATED", "INVALID", "DRAINING", "ERROR", "FAILED", "UNAVAILABLE":
+	case "DEPROVISIONING", "STOPPED", "STOPPING", "SUSPENDED", "SUSPENDING", "TERMINATED", "INVALID", "DRAINING", "ERROR", "FAILED", "UNAVAILABLE", "MAINTENANCE":
 		return bloopi_agent.StatusRed
 
-	case "PROVISIONING", "STAGING", "REPAIRING", "DELETING", "CREATING", "RECONCILING", "DEGRADED", "RUNNING_WITH_ERROR":
+	case "PROVISIONING", "STAGING", "REPAIRING", "DELETING", "CREATING", "RECONCILING", "DEGRADED", "RUNNING_WITH_ERROR", "PENDING_DELETE", "PENDING_CREATE":
 		return bloopi_agent.StatusOrange
 	}
 
