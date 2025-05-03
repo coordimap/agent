3188721c578bf1733884f5cfe0fb9cd1ea203930
Ermal Guni <ermalguni@gmail.com>
2025-05-03 14:34:30 +0200

    Fix cloud sql status and add version

--- internal/cloud/gcp/status.go
+++ internal/cloud/gcp/status.go
@@ -4,13 +4,13 @@ import "dev.azure.com/bloopi/bloopi/_git/shared_models.git/bloopi_agent"
 
 func getComputeStatus(status string) string {
 	switch status {
-	case "RUNNING", "READY", "RUNNABLE":
+	case "RUNNING", "READY", "RUNNABLE", "ALWAYS":
 		return bloopi_agent.StatusGreen
 
-	case "DEPROVISIONING", "STOPPED", "STOPPING", "SUSPENDED", "SUSPENDING", "TERMINATED", "INVALID", "DRAINING", "ERROR", "FAILED", "UNAVAILABLE", "MAINTENANCE":
+	case "DEPROVISIONING", "STOPPED", "STOPPING", "SUSPENDED", "SUSPENDING", "TERMINATED", "INVALID", "DRAINING", "ERROR", "FAILED", "UNAVAILABLE", "MAINTENANCE", "NEVER":
 		return bloopi_agent.StatusRed
 
-	case "PROVISIONING", "STAGING", "REPAIRING", "DELETING", "CREATING", "RECONCILING", "DEGRADED", "RUNNING_WITH_ERROR", "PENDING_DELETE", "PENDING_CREATE":
+	case "PROVISIONING", "STAGING", "REPAIRING", "DELETING", "CREATING", "RECONCILING", "DEGRADED", "RUNNING_WITH_ERROR", "PENDING_DELETE", "PENDING_CREATE", "ON_DEMAND":
 		return bloopi_agent.StatusOrange
 	}
 
