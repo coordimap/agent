10fc6b7f45db4a8955f0c0f0c9b6d218735f2d94
Ermal Guni <ermalguni@gmail.com>
2025-11-15 20:20:01 +0100

    Create an object for the mapping to better handle it

--- internal/cloud/utils/mappings.go
+++ internal/cloud/utils/mappings.go
@@ -60,3 +60,86 @@ func SplitConfiguredMappings(configuredMappings string) (map[string]string, erro
 
 	return mappings, nil
 }
+
+// MappingsInterface defines the interface for managing mappings.
+type MappingsInterface interface {
+	GetInternalName(mappingToSearchFor string) (string, error)
+	GetDataSourceID(mappingToSearchFor string) (string, error)
+	AddMapping(dataSourceID string, internalName string) error
+	AddConfiguredMapping(configuredMappings string) error
+}
+
+// Mappings holds the configured mappings
+type Mappings struct {
+	mappings map[string]string
+}
+
+// NewMappings creates a new Mappings object from a raw configuration string
+func NewMappings(configuredMappings string) (MappingsInterface, error) {
+	m := &Mappings{mappings: make(map[string]string)}
+	if err := m.AddConfiguredMapping(configuredMappings); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
+// GetInternalName returns the internal name for a given mapping
+func (m *Mappings) GetInternalName(mappingToSearchFor string) (string, error) {
+	val, ok := m.mappings[mappingToSearchFor]
+	if !ok {
+		return "", errors.New("mapping not found")
+	}
+	return fmt.Sprintf("%s-%s", val, mappingToSearchFor), nil
+}
+
+// GetDataSourceID returns the data source ID for a given mapping
+func (m *Mappings) GetDataSourceID(mappingToSearchFor string) (string, error) {
+	val, ok := m.mappings[mappingToSearchFor]
+	if !ok {
+		// Check if there is an asterisk (*) in any of the keys
+		for key, value := range m.mappings {
+			newKey := strings.Replace(key, "*", ".*", 1)
+			regex, errRegex := regexp.Compile(newKey)
+			if errRegex != nil {
+				return "", fmt.Errorf("could not create regex because %w", errRegex)
+			}
+
+			if regex.MatchString(mappingToSearchFor) {
+				return value, nil
+			}
+		}
+		return "", errors.New("mapping not found")
+	}
+	return val, nil
+}
+
+// AddMapping adds a new mapping if it doesn't already exist.
+func (m *Mappings) AddMapping(dataSourceID string, internalName string) error {
+	if _, ok := m.mappings[dataSourceID]; ok {
+		return fmt.Errorf("mapping for key '%s' already exists", dataSourceID)
+	}
+	m.mappings[dataSourceID] = internalName
+	return nil
+}
+
+// AddConfiguredMapping adds new mappings from a raw configuration string.
+func (m *Mappings) AddConfiguredMapping(configuredMappings string) error {
+	splitString := strings.Split(configuredMappings, " ")
+
+	for _, mapping := range splitString {
+		splitMapping := strings.Split(mapping, "@")
+
+		if len(splitMapping) != 2 {
+			continue
+		}
+
+		dataSourceID := splitMapping[0]
+		internalName := splitMapping[1]
+
+		if err := m.AddMapping(dataSourceID, internalName); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
